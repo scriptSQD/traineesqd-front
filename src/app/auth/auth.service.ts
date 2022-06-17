c87f2dccf4317404586446fbc3826383f2663f69
scriptSQD <73490413+scriptSQD@users.noreply.github.com>
2022-06-17 23:42:56 +0300

    Update CI/CD flow. (#4)
    * [I] - Move from async/await to RxJS.
    * [I] - Rename components and selectors to match Angular constraints.
    * [I] - Rename component @inputs to be less verbose.
    * [I] - Wrap router-outlet in div with common styling instead of styling each :host independently.
    * Enable CI to create environment files with GitHub Repo Secrets

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,86 +3,86 @@ import { Injectable } from "@angular/core";
 import { catchError, Observable, of, ReplaySubject, switchMap } from "rxjs";
 import { environment } from "src/environments/environment";
 import { ILogin, IRegister } from "./interfaces/auth.interface";
-import { User } from "./models/user.model";
+import { IUser, User } from "./models/user.model";
 import {
-    clearLocalJwt,
-    getLocalStorageJwt,
-    UpdateLocalJwt,
+	clearLocalJwt,
+	getLocalStorageJwt,
+	UpdateLocalJwt,
 } from "./utils/utils";
 
 type JwtAuth = {
-    jwt: string;
-    user: User;
+	jwt: string;
+	user: IUser;
 };
 
 @Injectable({
-    providedIn: "root",
+	providedIn: "root",
 })
 export class AuthService {
-    user$ = new ReplaySubject<User | null>();
-    jwt = getLocalStorageJwt();
-    isAuth$ = new ReplaySubject<boolean>();
+	user$ = new ReplaySubject<User | null>();
+	jwt = getLocalStorageJwt();
+	isAuth$ = new ReplaySubject<boolean>();
 
-    constructor(private readonly http: HttpClient) {
-        if (!this.jwt) return;
+	constructor(private readonly http: HttpClient) {
+		if (!this.jwt) return;
 
-        this.http
-            .get<User>(`${environment.backend_url}/users/me`, {
-                headers: { Authorization: `Bearer ${this.jwt}` },
-            })
-            .subscribe({
-                next: (res: User) => {
-                    this.user$.next(res);
-                    this.isAuth$.next(true);
-                },
-                error: () => {
-                    clearLocalJwt();
-                    this.isAuth$.next(false);
-                },
-            });
-    }
+		this.http
+			.get<IUser>(`${environment.backend_url}/users/me`, {
+				headers: { Authorization: `Bearer ${this.jwt}` },
+			})
+			.subscribe({
+				next: (res: IUser) => {
+					this.user$.next(new User(res));
+					this.isAuth$.next(true);
+				},
+				error: () => {
+					clearLocalJwt();
+					this.isAuth$.next(false);
+				},
+			});
+	}
 
-    login(payload: ILogin): Observable<HttpErrorResponse | null> {
-        return this.http
-            .post<JwtAuth>(`${environment.backend_url}/auth/login`, {
-                ...payload,
-            })
-            .pipe(
-                switchMap(res => {
-                    this.jwt = UpdateLocalJwt(res.jwt);
-                    this.user$.next(res.user);
-                    this.isAuth$.next(true);
+	login(payload: ILogin): Observable<HttpErrorResponse | null> {
+		return this.http
+			.post<JwtAuth>(`${environment.backend_url}/auth/login`, {
+				...payload,
+			})
+			.pipe(
+				switchMap(res => {
+					this.jwt = UpdateLocalJwt(res.jwt);
+					this.user$.next(new User(res.user));
+					this.isAuth$.next(true);
 
-                    return of(null);
-                }),
-                catchError(err => {
-                    return of(err);
-                })
-            );
-    }
+					return of(null);
+				}),
+				catchError(err => {
+					return of(err);
+				})
+			);
+	}
 
-    register(payload: IRegister): Observable<void> {
-        return this.http
-            .post<JwtAuth>(`${environment.backend_url}/auth/register`, {
-                ...payload,
-            })
-            .pipe(
-                switchMap(res => {
-                    if (!res) throw new HttpErrorResponse({ error: res });
+	register(payload: IRegister): Observable<void> {
+		return this.http
+			.post<JwtAuth>(`${environment.backend_url}/auth/register`, {
+				...payload,
+			})
+			.pipe(
+				switchMap(res => {
+					if (!res) throw new HttpErrorResponse({ error: res });
 
-                    this.jwt = UpdateLocalJwt(res.jwt);
-                    this.user$.next(res.user);
-                    this.isAuth$.next(true);
+					this.jwt = UpdateLocalJwt(res.jwt);
+					this.user$.next(new User(res.user));
+					this.isAuth$.next(true);
 
-                    return of(undefined);
-                })
-            );
-    }
+					return of(undefined);
+				})
+			);
+	}
 
-    logout(): void {
-        clearLocalJwt();
-        this.jwt = null;
-        this.isAuth$.next(false);
-        this.user$.next(null);
-    }
+	logout(): void {
+		clearLocalJwt();
+		this.jwt = null;
+		this.isAuth$.next(false);
+		this.user$.next(null);
+	}
 }
